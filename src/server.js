55d373bf0ebb8bc77db74679307fb2fe3e85551c
Swadha <swadhagupta.27@gmail.com>
2026-06-23 23:49:10 +0530

    added change to host game on local network

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 // require("dotenv").config();
 const http = require("http");
+const os = require("os");
 const app = require("./app.js"); // Import Express app
 const { setupSocket } = require("./socket/socketHandler.js");
 const { logger, currentLevelName } = require("./utils/logger");
@@ -12,8 +13,22 @@ const server = http.createServer(app);
 // Initialize Socket.io
 setupSocket(server);
 
-server.listen(PORT, () => {
-  logger.info(`Server running on http://localhost:${PORT}`, {
-    logLevel: currentLevelName,
-  });
+// Non-internal IPv4 addresses, so we can print the URL to share over the LAN.
+function lanAddresses() {
+  const addrs = [];
+  for (const ifaces of Object.values(os.networkInterfaces())) {
+    for (const iface of ifaces || []) {
+      if (iface.family === "IPv4" && !iface.internal) addrs.push(iface.address);
+    }
+  }
+  return addrs;
+}
+
+// Bind to 0.0.0.0 so other devices on the same WiFi/LAN can reach the server.
+server.listen(PORT, "0.0.0.0", () => {
+  logger.info(`Server listening on port ${PORT}`, { logLevel: currentLevelName });
+  logger.info(`Local:   http://localhost:${PORT}`);
+  for (const ip of lanAddresses()) {
+    logger.info(`Network: http://${ip}:${PORT}  <- share this on your LAN`);
+  }
 });
